c8e3203868e6c08e95fb43dcfa3fe8cbdbb91324
Parthiban Srinivasan <parthiban.srinivasan@gmail.com>
2016-05-31 03:36:07 +0000

    Greeter initial

---
+++ greeter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+        "log"
+
+        "github.com/micro/go-micro"
+        proto "github.com/parthiban-srinivasan/mserv/proto"
+
+        "golang.org/x/net/context"
+)
+
+type Greeter struct{}
+
+func (gr *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+        rsp.Greeting = "Hello " + req.Name
+        return nil
+}
+
+func main() {
+        service := micro.NewService(
+                micro.Name("greeter"),
+                micro.Version("0.0"),
+        )
+
+        service.Init()
+
+        proto.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+        if err := service.Run(); err != nil {
+                log.Fatal(err)
+        }
+}
