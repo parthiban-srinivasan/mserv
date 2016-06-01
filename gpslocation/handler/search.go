9fbca05d43ff87cf6e3b9683114b100337869cab
Parthiban Srinivasan <parthiban.srinivasan@icloud.com>
2016-06-01 19:24:18 +0000

    post debug log

--- gpslocation/handler/search.go
+++ gpslocation/handler/search.go
@@ -12,14 +12,19 @@ import (
 
 func (loc *GpsLocation) Search(ctx context.Context, req *proto.SearchRequest, rsp *proto.SearchResponse) error {
 	log.Print("Received Location.Search request")
+	log.Print(req)
 
 	entity := &domain.Entity{
 		Latitude:  req.Center.Latitude,
 		Longitude: req.Center.Longitude,
 	}
 
+	log.Print("domain entity:", entity)
+	
 	entities := dao.Search(req.Type, entity, req.Radius, int(req.NumEntities))
 
+	log.Print("entities:", entities)
+	
 	for _, e := range entities {
 		rsp.Entities = append(rsp.Entities, e.ToProto())
 	}
