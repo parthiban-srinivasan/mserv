e39644501742735b5b1d15dc8a65b3dc148688bf
Parthiban Srinivasan <parthiban.srinivasan@gmail.com>
2016-06-07 04:26:57 +0000

    micro query

---
+++ gpslocation/client/cli.sh
@@ -0,0 +1,6 @@
+
+micro query go.micro.srv.gpsloc GpsLocation.Get '{"Id": "id123"}'
+
+micro query go.micro.srv.gpsloc GpsLocation.Search '{"center": {"latitude": 51.5
+165, "longitude": 0.1246, "timestamp": 145443}, "radius": 2, "type": "Hotel", "numEntities": 1}'
+
